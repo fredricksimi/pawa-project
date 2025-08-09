654942de2b7e30a93ebda65ce25ad61d2454d8c8
FredrickSimi <fkemosi@gmail.com>
2025-08-09 17:36:07 +0300

    added config

--- main.tf
+++ main.tf
@@ -32,13 +32,7 @@ resource "google_project_service" "cloud_run_api" {
   disable_on_destroy = false
 }
 
-resource "google_artifact_registry_repository" "my_repo" {
-  location      = var.gcp_region
-  repository_id = var.repo_name
-  description   = "My docker repository"
-  format        = "DOCKER"
-  depends_on = [google_project_service.artifact_registry_api,]
-}
+
 
 resource "google_cloud_run_v2_service" "default" {
   name     = "cloudrun-service"

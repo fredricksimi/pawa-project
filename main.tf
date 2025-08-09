521a97fc91204a687b943945233cd1058a360c76
FredrickSimi <fkemosi@gmail.com>
2025-08-09 14:34:24 +0300

    added sa

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "google" {
   project = var.gcp_project_id
   region = var.gcp_region
   zone = var.gcp_zone
-  credentials = "gcp_credentials.json"
+  #credentials = "gcp_credentials.json"
 }
 
 resource "google_project_service" "artifact_registry_api" {
